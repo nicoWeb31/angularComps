d3df28f30447d08ea8aa0663ec324ffc143a8a27
Riot Nicolas <nico.riot@free.fr>
2020-05-27 10:32:38 +0200

    route home and not found

--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ElementHomeComponent} from './element-home/element-home.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'elements', component :ElementHomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forChild(routes)],
+exports: [RouterModule]
 })
 export class ElementsRoutingModule { }
--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementHomeComponent } from './element-home/element-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ElementHomeComponent],
   imports: [
     CommonModule,
     ElementsRoutingModule
-  ]
+  ],
+  //find to export
+  exports :[]
 })
 export class ElementsModule { }
